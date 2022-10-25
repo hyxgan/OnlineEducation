f368be3e42200a3b639226c1d9218cc0b3333b1e
hyxgan <2425834738@qq.com>
2022-10-25 20:22:56 +0800

    gitee first commit

---
+++ gitee-test/src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @auther hyx
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("gitee");
+        System.out.println("gitee");
+        System.out.println("gitee");
+        System.out.println("gitee");
+    }
+
+}
